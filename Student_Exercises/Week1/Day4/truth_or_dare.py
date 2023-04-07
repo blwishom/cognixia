a70e00699cdcc66cd301ebaa9f613899c6013530
Blair Wishom <blwishom@gmail.com>
2023-04-06 19:09:41 -0700

    updated games functionality

--- Student_Exercises/Week1/Day4/truth_or_dare.py
+++ Student_Exercises/Week1/Day4/truth_or_dare.py
@@ -6,9 +6,9 @@ print(' ')
 def truth_or_dare():
 
 
-    truths = ['When was the last time you picked your nose?', 'Have you ever put too much dip on your chip?']
-    dares = ['I dare you to put milk in a bag of chips and eat them!', 'I dare you to tell someone they have a really nice forehead!']
-    play_again = 'yes'
+    truths = ['When was the last time you picked your nose?', 'Have you ever put too much dip on your chip?', 'Have you ever faked sick to miss school or work?', 'Do you shower everyday?', 'What is the longest period of time you\'ve went without leaving the house?']
+    dares = ['Put milk in a bag of chips and eat them!', 'Tell someone they have a really nice forehead!', 'Open your window and yell "HELPPPPP!"', 'Do 10 push-ups!', 'Call a friend and tell them you wet the bed last night!']
+    play_again = 'play'
 
     # if (select_truth_or_dare == 'quit'):
     #     print(' ')
@@ -17,43 +17,38 @@ def truth_or_dare():
     # if (select_truth_or_dare == 'dare') or (select_truth_or_dare == 'truth'):
     #     playing = True
 
-    while (play_again == 'yes'):
+    while (play_again == 'play'):
         print(' ')
         select_truth_or_dare = input('Would you like to choose a truth or a dare? ')
-        print(' ')
 
-        if (play_again == 'no'):
+        if (play_again == 'quit'):
             print(' ')
             print('Thanks for playing!')
             break
-            play_again = input('Type "yes" to play again or "no" to quit: ')
         elif (select_truth_or_dare == 'dare'):
             print(' ')
             print('Here is your dare: ', random.choice(dares))
             print(' ')
-            play_again = input('Type "yes" to play again or "no" to quit: ')
-            if (play_again == 'no'):
+            play_again = input('Type "play" to play again or "quit" to quit: ')
+            if (play_again == 'quit'):
                 print(' ')
                 print('Thanks for playing!')
                 break
-            else:
-                truth_or_dare()
         elif (select_truth_or_dare != 'truth'):
             print(' ')
             print('Please enter truth or dare for your response')
             print(' ')
             truth_or_dare()
+
         elif (select_truth_or_dare == 'truth'):
             print(' ')
             print('Tell the truth: ', random.choice(truths))
             print(' ')
-            play_again = input('Type "yes" to play again or "no" to quit: ')
-            if (play_again == 'no'):
+            play_again = input('Type "play" to play again or "quit" to quit: ')
+            if (play_again == 'quit'):
                 print(' ')
                 print('Thanks for playing!')
                 break
-            else:
-                truth_or_dare()
         elif (select_truth_or_dare != 'dare'):
             print(' ')
             print('Please enter truth or dare for your response')
