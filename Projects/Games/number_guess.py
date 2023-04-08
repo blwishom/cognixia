2117e8ae99c75ed22f8c831922eef4aa83ee82cb
Blair Wishom <blwishom@gmail.com>
2023-04-08 15:36:44 -0700

    clean up number guess

--- Projects/Games/number_guess.py
+++ Projects/Games/number_guess.py
@@ -5,53 +5,55 @@ print(' ')
 print('Let\'s play a guessing game!')
 print(' ')
 def number_guess():
-    guesses = 0
     num = random.randint(1,10)
-    user_input = int(input('Guess a number between 1 and 10: '))
+    guessing = True
 
-    if user_input == num:
-        print(' ')
-        print(f'You guessed it! The number was {num}!')
-        print(' ')
-    elif user_input < num:
-        print(' ')
-        print('Try again! Your guess was too low.')
-        print(' ')
-        number_guess()
-    elif user_input > num:
-        print(' ')
-        print('Try again! Your guess was too high.')
-        print(' ')
-        number_guess()
+    while guessing:
+        user_input = int(input('Guess a number between 1 and 10: '))
 
-    play_again_input = input('Type "play" to play again or "quit" to quit the game: ')
-    print(' ')
-
-    if play_again_input == 'play':
-        number_guess()
-    if play_again_input == 'quit':
-        print(' ')
-        print('Thanks for playing!')
-        print(' ')
-        sys.exit()
-    elif (play_again_input != 'play') or (play_again_input != 'quit'):
-        print(' ')
-        print('WARNING: If you type anything other than "yes" to play again or "no" to quit you will be exited from the game')
-        print(' ')
-        play_again_input = input('Type "play" to play again or "quit" to quit the game: ')
-        print(' ')
-        if play_again_input == 'play':
+        if user_input < num:
+            print(' ')
+            print('Try again! Your guess was too low.')
+            print(' ')
+            number_guess()
+        elif user_input > num:
+            print(' ')
+            print('Try again! Your guess was too high.')
+            print(' ')
             number_guess()
         else:
+            print(' ')
+            print(f'You guessed it! The number was {num}!')
+            print(' ')
+
+        play_again_input = input('Type "play" to play again or "quit" to quit the game: ')
+        print(' ')
+
+        if play_again_input == 'play':
+            number_guess()
+        if play_again_input == 'quit':
+            print(' ')
             print('Thanks for playing!')
             print(' ')
             sys.exit()
+        elif (play_again_input != 'play') or (play_again_input != 'quit'):
+            print(' ')
+            print('WARNING: If you type anything other than "yes" to play again or "no" to quit you will be exited from the game')
+            print(' ')
+            play_again_input = input('Type "play" to play again or "quit" to quit the game: ')
+            print(' ')
+            if play_again_input == 'play':
+                number_guess()
+            else:
+                print('Thanks for playing!')
+                print(' ')
+                sys.exit()
 
-    if play_again_input == 'quit':
-        print(' ')
-        print('Thanks for playing!')
-        print(' ')
-        sys.exit()
+        if play_again_input == 'quit':
+            print(' ')
+            print('Thanks for playing!')
+            print(' ')
+            sys.exit()
 
 number_guess()
 
