dcb109e3fa8854e0fc26c8dda4ff04abed197577
Jacob Tanner <jdtanner@mail.roanoke.edu>
2023-04-12 10:54:58 -0400

    UI branch with some Pseudocode

---
+++ ui.py
@@ -0,0 +1,19 @@
+'''
+import employee
+import department
+
+
+while True:
+
+    try except block that offers option to create, read, update, or delete both Employee and Department
+    
+        create employee called from Department
+        create department called from Department
+        read, update, delete for employee and department called from respective classes
+
+That all?  Can't think of anything else
+
+
+
+
+'''
