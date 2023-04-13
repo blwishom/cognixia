a797f85439bcf9fdce935b930ee9ca98c706a958
Jacob Tanner <jdtanner@mail.roanoke.edu>
2023-04-13 17:00:57 -0400

    ui changes

--- models/ui.py
+++ models/ui.py
@@ -1,5 +1,5 @@
 from department import Department
-from db import create_db, view_departments, view_department, insert_department, update_department, delete_department, view_employees, delete_employee, update_employee
+from db import create_db, view_departments, view_department, insert_department, update_department, delete_department, view_employees, delete_employee, update_employee, view_employee
 import datetime
 
 def print_menu():
@@ -63,9 +63,24 @@ while loop:
     elif choice == '7':
         view_employees()
     elif choice == '8':
-        id = input("Enter the ID of the employee to delete: ")
-        payload = {'id': id}
-        delete_employee(payload)
+        # id = input("Enter the ID of the employee to delete: ")
+        temp_name = input("Enter the Last Name of the employee to delete: ")
+        temp_empl = view_employee(temp_name)
+        # print(type(temp_empl))
+        temp_sal = temp_empl[4]
+        # print(temp_sal)
+
+        temp_empl_dict = (("id",str(temp_empl[0])),('fname',temp_empl[1]),('lname',temp_empl[2]),('doe',temp_empl[3]),('salary',temp_empl[4]),('department',temp_empl[5]))
+        temp_empl_dict = dict(temp_empl_dict)
+        # print(temp_empl_dict)
+        temp_dept = view_department(temp_empl[5])
+        temp_dept_dict = (('id',temp_dept[0]),('name',temp_dept[1]),('employee_count',temp_dept[2]-1),('department_domain',temp_dept[3]),('labor_cost',temp_dept[4]))
+        temp_dept_dict = dict(temp_dept_dict)
+        temp_dept_dict["labor_cost"] = temp_dept_dict['labor_cost'] - temp_sal
+        update_department(temp_dept_dict)
+
+        # payload = {'id': id}
+        delete_employee(temp_empl_dict)
         print("Employee deleted successfully.")
     elif choice == '9':
         id = input("Enter the ID of the employee to update: ")
@@ -73,21 +88,15 @@ while loop:
         lname = input("Enter the new last name of the employee: ")
         doe = datetime.date.today()
         salary = input("Enter the new salary of the employee: ")
-        while(True):
-            department_name = input("Enter the name of the department to hire an employee: ")
-            department = view_department(department_name)
-            if department:
-                break
-            else:
-                True
-        payload = {'id': id, 'fname': fname, 'lname': lname, 'doe': doe, 'salary': salary, 'department' : department_name}
+        department = input("Enter the new department of the employee: ")
+        payload = {'id': id, 'fname': fname, 'lname': lname, 'doe': doe, 'salary': salary, 'department' : department}
         update_employee(payload)
-        print("Employee updated successfully.")
+        print("Department updated successfully.")
     elif choice == '0':
         loop = False
     else:
 
         print("Invalid choice. Please enter a number from 1 to 8.")
 
-print_menu()
+# print_menu()
 
