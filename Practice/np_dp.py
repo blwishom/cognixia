80b708a72cb2ea8dc69a5ca69dc624a067f24e57
Blair Wishom <blwishom@gmail.com>
2023-04-22 18:54:06 -0700

    numpy creating arrays practice

--- Practice/np_dp.py
+++ Practice/np_dp.py
@@ -2,18 +2,26 @@ import numpy as np
 import pandas as pd
 
 # NUMPY CREATING ARRAYS
-arr1 = np.array(['yes', 'no', 'maybe so', 'uhhhh'])
-print(arr1)
+arr1 = np.array(['yes', 'no', 'maybe so', 'uhhhh', 1, 2, 3, 4, 3.0])
+arr2 = np.array([ 1, 2, 3, 4, 3.0, 'yes', 'no', 'maybe so', 'uhhhh'])
+arr3 = np.array([1, 2, 3, 4])
+print(arr1, '<----', arr1.dtype)
+print(arr2, '<----', arr2.dtype)
+print(arr3, '<----', arr3.dtype)
 
-zeros = np.zeros(4 + 2)
-print(zeros)
+# print(arr1.dtype)
+# print(arr2.dtype)
+# print(arr3.dtype)
 
-empty = np.empty(4)
-print(empty)
+# zeros = np.zeros(4 + 2)
+# print(zeros)
 
-range1 = np.arange(5)
-range2 = np.arange(5, 15)
-range3 = np.arange(5, 25, 5)
-print(range1)
-print(range2)
-print(range3)
+# empty = np.empty(4)
+# print(empty)
+
+# range1 = np.arange(5)
+# range2 = np.arange(5, 15)
+# range3 = np.arange(2, 41, 6)
+# print(range1)
+# print(range2)
+# print(range3)
