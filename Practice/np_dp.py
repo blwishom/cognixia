9abe741015c07beb220471a454b74b0b868935d3
Blair Wishom <blwishom@gmail.com>
2023-04-23 12:38:24 -0700

    series head/tail and conditions

--- Practice/np_dp.py
+++ Practice/np_dp.py
@@ -63,10 +63,13 @@ import pandas as pd
 # print(arr3.max())
 
 
-# PANDAS
+# ********************     PANDAS     ********************
 series1 = pd.Series([5, 4, 3, 2, 1])
 series2 = pd.Series({5: 'five', 4: 'four', 3: 'three', 'two': 'two or 2', 1: 'one'})
 series3 = pd.Series({'one': 1, 'two': 2, 'three': 3, 'four': '4 or four', 'five': '5 or five'})
+series4 = pd.Series(['uno', 'dos', 'tres', 'quatro'])
+
+# LOC and ILOC
 Iloc = series1.iloc[0]
 Loc = series2.loc[3]
 print(Iloc)
@@ -78,3 +81,24 @@ Iloc2 = series3.iloc[4]
 loc2 = series3['four']
 print(Iloc2)
 print(loc2)
+
+print('============================')
+
+# HEAD and TAIL
+head = series4.head(2)
+head2 = series3.head(2)
+print(head, '<---HEAD')
+print(head2, '<---HEAD 2')
+
+tail = series2.tail(3)
+tail2 = series4.tail(2)
+print(tail, '<---TAIL')
+print(tail2, '<---TAIL 2')
+
+print('============================')
+
+# CONDITION
+condition = series1[series1 < 3]
+condition2 = series3[series3 != 2]
+print(condition, '<---CONDITION')
+print(condition2, '<---CONDITION 2')
