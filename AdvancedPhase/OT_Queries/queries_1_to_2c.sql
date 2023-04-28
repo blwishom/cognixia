97717f4f9ed672693d5892ba9e373179613c1b10
Blair Wishom <blwishom@gmail.com>
2023-04-28 12:39:49 -0700

    ot queries 1-2c

---
+++ AdvancedPhase/OT_Queries/queries_1_to_2c.sql
@@ -0,0 +1,48 @@
+use ot;
+
+# Query 1a
+SELECT region_id as Region_ID, count(*) as Count
+FROM countries
+GROUP BY region_id
+ORDER BY Count desc;
+
+# Query 1b
+SELECT region_name as Region_With_Most_Locations
+FROM regions
+WHERE region_id = 1;
+
+# Query 1c
+SELECT state, city, postal_code
+FROM locations
+WHERE country_id != 'US' AND city LIKE 's%';
+
+# Query 1d
+SELECT *
+FROM locations
+WHERE state IS null;
+
+# Query 1e
+SELECT DISTINCT count(country_id) as Countries_With_Loctions
+FROM locations;
+
+# Query 2a
+SELECT product_name as Product_Name, list_price as List_Price
+FROM products
+WHERE list_price between 100 and 150;
+
+# Query 2b
+SELECT products.product_name as Product_Name, products.list_price as List_Price, product_categories.category_name as Category_Name
+FROM products
+JOIN product_categories
+	ON products.product_id = product_categories.category_id;
+
+# Query 2c
+SELECT products.product_name, products.list_price, quantity
+FROM products
+LEFT JOIN inventories
+	ON products.product_id = inventories.warehouse_id
+LEFT JOIN product_categories
+	ON inventories.quantity = product_categories.category_id
+RIGHT JOIN warehouses
+	ON warehouses.warehouse_id = warehouses.warehouse_id
+WHERE quantity > 100;
