798e171d3b9c9bd2310aa4222b2dee877690d03d
Blair Wishom <blwishom@gmail.com>
2023-04-29 11:34:20 -0700

    ot queries 2c

--- AdvancedPhase/mySQL_Queries/joins_and_subsqueries_exercise.sql
+++ AdvancedPhase/mySQL_Queries/joins_and_subsqueries_exercise.sql
@@ -1,46 +1,43 @@
 
 -- ************************SEE JOINS AND SUBQUERIES EXERCISE PNG FOR QUERY QUESTIONS************************
 
--- use sakila;
+use sakila;
 
--- -- select * from category;
--- -- select * from film;
+-- select * from category;
+-- select * from film;
 
 -- # Query #1
--- SELECT title as Title, rating as Rating, name as Category_Name
--- FROM film
--- LEFT JOIN category
--- 	ON film.film_id = category.category_id
--- WHERE rating != 'PG-13';
+SELECT title as Title, rating as Rating, name as Category_Name
+FROM film
+LEFT JOIN category
+	ON film.film_id = category.category_id
+WHERE rating != 'PG-13';
 
 -- # Query #2
--- SELECT first_name as First_Name, last_name as Last_Name, count(film_id) as Total_Films
--- FROM actor
--- JOIN film_actor
--- 	ON actor.actor_id = film_actor.film_id
--- GROUP BY film_actor.film_id
--- ORDER BY Total_Films desc;
+SELECT first_name as First_Name, last_name as Last_Name, count(film_id) as Total_Films
+FROM actor
+JOIN film_actor
+	ON actor.actor_id = film_actor.film_id
+GROUP BY film_actor.film_id
+ORDER BY Total_Films desc;
 
 -- # Query #3
--- SELECT title as Title, sum(inventory.film_id) as Inventory
--- FROM film
--- JOIN inventory
--- 	ON film.film_id = inventory.film_id
--- GROUP BY inventory.film_id;
+SELECT title as Title, sum(inventory.film_id) as Inventory
+FROM film
+JOIN inventory
+	ON film.film_id = inventory.film_id
+GROUP BY inventory.film_id;
 
 -- # Query #4
--- SELECT avg(replacement_cost)
--- FROM film
--- WHERE rating = 'PG-13';
+SELECT avg(replacement_cost)
+FROM film
+WHERE rating = 'PG-13';
 
--- SELECT title as Title, replacement_cost as Replacement_Cost_Above_Avg
--- FROM film
--- WHERE replacement_cost > (
--- 	SELECT avg(replacement_cost)
--- 	FROM film
--- 	WHERE rating = 'PG-13'
---     )
--- ORDER BY replacement_cost;
-
-
-
+SELECT title as Title, replacement_cost as Replacement_Cost_Above_Avg
+FROM film
+WHERE replacement_cost > (
+	SELECT avg(replacement_cost)
+	FROM film
+	WHERE rating = 'PG-13'
+    )
+ORDER BY replacement_cost;
