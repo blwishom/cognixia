16ccc1d4a091e8d142bdd0e86113447827afe895
Blair Wishom <blwishom@gmail.com>
2023-05-02 16:59:08 -0700

    digital mindset wk4 project 2

--- DigitalMindsetExercises/wk4_project.sql
+++ DigitalMindsetExercises/wk4_project.sql
@@ -18,3 +18,22 @@ INSERT INTO house VALUES(2, '4402 Digital Cir Mindset, Ca 56789', 5, 3.5, 1800);
 SELECT *
 FROM house
 WHERE house_id = 1;
+
+-- PROJECT 2
+use sakila;
+
+SELECT *
+FROM city;
+
+SELECT city as City
+FROM city;
+
+SELECT title as Title, rental_rate as Daily_Rental_Rate, rental_rate * 7 as Weekly_Rental_Rate
+FROM film;
+
+SELECT DISTINCT rating as Rating
+FROM film;
+
+SELECT round(avg(length(title)), 2) as Avg_Length_of_G_Rated_Titles
+FROM film
+WHERE title LIKE 'G%';
