b00eb574f916d470b56e6b96ba2b44ebece1f131
Blair Wishom <blwishom@gmail.com>
2023-05-31 16:49:04 -0700

    double num list comp

---
+++ Practice/lists.py
@@ -0,0 +1,12 @@
+nums = [4, 8, 15, 16, 23, 42]
+## LIST COMP
+double_nums = [num*2 for num in nums]
+
+## FOR LOOP
+# double_nums = []
+# for num in nums:
+#   double_nums.append(num*2)
+
+print(double_nums)
+
+
