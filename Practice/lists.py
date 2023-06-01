cf21b0bbf119e92b7e8470b1e4af50ea017991cb
Blair Wishom <blwishom@gmail.com>
2023-06-01 16:12:57 -0700

    booleans list comp

--- Practice/lists.py
+++ Practice/lists.py
@@ -62,3 +62,6 @@ names = ["Elaine", "George", "Jerry", "Cosmo"]
 is_Jerry = [name is "Jerry" for name in names]
 print(is_Jerry)
 
+nums = [5, -10, 40, 20, 0]
+greater_than_two = [num > 2 for num in nums]
+print(greater_than_two)
