485701df0daaee9e6fa55530533ac4830527b850
Blair Wishom <blwishom@gmail.com>
2023-06-02 18:21:54 -0700

    lambda functions

---
+++ Practice/lamdas.py
@@ -0,0 +1,11 @@
+contains_a = lambda word: 'a' in word
+
+print(contains_a("banana"))
+print(contains_a("apple"))
+print(contains_a("cherry"))
+
+
+long_string = lambda str: len(str) > 12
+
+print(long_string("short"))
+print(long_string("photosynthesis"))
