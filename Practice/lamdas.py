0d64566402c06129dfce7b3c29ce1bd90e1191e6
Blair Wishom <blwishom@gmail.com>
2023-06-02 18:23:54 -0700

    lambda functions check strings

--- Practice/lamdas.py
+++ Practice/lamdas.py
@@ -9,3 +9,10 @@ long_string = lambda str: len(str) > 12
 
 print(long_string("short"))
 print(long_string("photosynthesis"))
+
+
+ends_in_a = lambda str: str.endswith('a')
+# ends_in_a = lambda str: str[-1] is 'a'
+
+print(ends_in_a("data"))
+print(ends_in_a("aardvark"))
