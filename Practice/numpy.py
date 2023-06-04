7281fbc25fe1d1d1026bdfb07055c353933a3397
Blair Wishom <blwishom@gmail.com>
2023-06-03 22:18:02 -0700

    addition to each element in the array

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -6,3 +6,12 @@ print(test_1)
 # import from csv file as an array
 test_2 = np.genfromtxt('test_2.csv', delimiter=',')
 print(test_2)
+
+
+# add 2 to each element in the array
+test_1 = np.array([92, 94, 88, 91, 87])
+test_2 = np.array([79, 100, 86, 93, 91])
+test_3 = np.array([87, 85, 72, 90, 92])
+
+test_3_fixed = test_3 + 2
+print(test_3_fixed)
