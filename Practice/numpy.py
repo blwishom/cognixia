88653b176d536516ff32ac61f6275c221d5d2c99
Blair Wishom <blwishom@gmail.com>
2023-06-05 19:09:57 -0700

    import and read csv file data, alter data at each index, index arrays, logical operators

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -71,3 +71,19 @@ just_right = porridge[(porridge > 60) & (porridge < 80)]
 print(cold)
 print(hot)
 print(just_right)
+
+
+# import and read csv file data, alter data at each index, index arrays, logical operators
+temperatures = np.genfromtxt('temperature_data.csv', delimiter=',')
+print(temperatures)
+
+temperatures_fixed = temperatures+3
+
+monday_temperatures = temperatures_fixed[0]
+print(monday_temperatures)
+
+thursday_friday_morning = temperatures_fixed[3:, 1]
+print(thursday_friday_morning)
+
+temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
+print(temperature_extremes)
