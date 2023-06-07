c2e0acbe0e6fbf1aca12448ac5e5b877d59359c9
Blair Wishom <blwishom@gmail.com>
2023-06-06 17:01:05 -0700

    BETTYS BAKERY PROJECT

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -87,3 +87,26 @@ print(thursday_friday_morning)
 
 temperature_extremes = temperatures_fixed[(temperatures_fixed < 50) | (temperatures_fixed > 60)]
 print(temperature_extremes)
+
+
+# BETTY'S BAKERY PROJECT
+# Recipe	Cups of Flour	Cups of Sugar	Eggs	Cups of Milk	Cups of Butter
+# Cupcakes	…	…	…	…	…
+# Pancake	…	…	…	…	…
+# Cookie	…	…	…	…	…
+# Bread	…	…	…	…	…
+
+cupcakes = np.array([2, 0.75, 2, 1, 0.5])
+print(cupcakes)
+recipes = np.genfromtxt('recipes.csv', delimiter=',')
+print(recipes)
+eggs = recipes[:, 2]
+print(eggs)
+one_egg = recipes[:, 2] == 1
+print(one_egg)
+cookies = recipes[2, :]
+print(cookies)
+double_batch = cupcakes*2
+print(double_batch)
+grocery_list = cookies + double_batch
+print(grocery_list)
