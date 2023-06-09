a6bdcd0d1d7bfe241256c22ccfc1ffdaf05a04cc
Blair Wishom <blwishom@gmail.com>
2023-06-09 15:54:18 -0700

    2-D array means

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -146,3 +146,18 @@ class_year = np.array([1967, 1949, 2004, 1997, 1953, 1950, 1958, 1974, 1987, 200
 
 millennials = np.mean(class_year > 2005)
 print(millennials)
+
+
+# 2-D ARRAY MEANS
+allergy_trials = np.array([[6, 1, 3, 8, 2],
+                           [2, 6, 3, 9, 8],
+                           [5, 2, 6, 9, 9]])
+
+total_mean = np.mean(allergy_trials)
+print(total_mean)
+
+trial_mean = np.mean(allergy_trials, axis=1)
+print(trial_mean)
+
+patient_mean = np.mean(allergy_trials, axis=0)
+print(patient_mean)
