5f845bfc1007515c81fda1ed1e9df8f8a2abfe3d
Blair Wishom <blwishom@gmail.com>
2023-06-09 16:11:03 -0700

    find median of data sets

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -161,3 +161,19 @@ print(trial_mean)
 
 patient_mean = np.mean(allergy_trials, axis=0)
 print(patient_mean)
+
+
+# SORT ARRAY TO FIND OUTLIERS
+temps = np.array([86, 88, 94, 85, 97, 90, 87, 85, 94, 93, 92, 95, 98, 85, 94, 91, 97, 88, 87, 86, 99, 89, 89, 99, 88, 96, 93, 96, 85, 88, 191, 95, 96, 87, 99, 93, 90, 86, 87, 100, 187, 98, 101, 101, 96, 94, 96, 87, 86, 92, 98,94, 98, 90, 99, 96, 99, 86, 97, 98, 86, 90, 86, 94, 91, 88, 196, 195,93, 97, 199, 87, 87, 90, 90, 98, 88, 92, 97, 88, 85, 94, 88, 93, 198, 90, 91, 90, 92, 92])
+
+sorted_temps = np.sort(temps)
+print(sorted_temps)
+
+# FIND MEDIAN OF DATA SETS
+large_set = np.genfromtxt('household_income.csv', delimiter=',')
+
+small_set_median = 40500
+print(small_set_median)
+
+large_set_median = np.median(large_set)
+print(large_set_median)
