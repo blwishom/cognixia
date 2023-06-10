477167eef6f7a1978f06b72d0a6ca6a6a4d40c1d
Blair Wishom <blwishom@gmail.com>
2023-06-10 12:19:31 -0700

    numpy standard deviation

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -201,3 +201,21 @@ print(third_quarter)
 
 interquartile_range = third_quarter - first_quarter
 print(interquartile_range)
+
+
+# finding Standard Deviation
+pumpkin = np.array([68, 1820, 1420, 2062, 704, 1156, 1857, 1755, 2092, 1384])
+
+acorn_squash = np.array([20, 43, 99, 200, 12, 250, 58, 120, 230, 215])
+
+pumpkin_avg = np.mean(pumpkin)
+print(pumpkin_avg)
+
+acorn_squash_avg = np.mean(acorn_squash)
+print(acorn_squash_avg)
+
+pumpkin_std = np.std(pumpkin)
+print(pumpkin_std)
+
+acorn_squash_std = np.std(acorn_squash)
+print(acorn_squash_std)
