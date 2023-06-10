97d5b00c7c6d6422e427fcb4532c822aac9bee25
Blair Wishom <blwishom@gmail.com>
2023-06-10 12:05:44 -0700

    numpy percentiles/quartiles

--- Practice/numpy.py
+++ Practice/numpy.py
@@ -188,3 +188,16 @@ print(thirtieth_percentile)
 
 seventieth_percentile = np.percentile(patrons, 70)
 print(seventieth_percentile)
+
+
+# finding quartiles (first, third, and inner(75th - 25th))
+movies_watched = np.array([2, 3, 8, 0, 2, 4, 3, 1, 1, 0, 5, 1, 1, 7, 2])
+
+first_quarter = np.percentile(movies_watched, 25)
+print(first_quarter)
+
+third_quarter = np.percentile(movies_watched, 75)
+print(third_quarter)
+
+interquartile_range = third_quarter - first_quarter
+print(interquartile_range)
