aa1fe65314abfe44817086203b847568c3db261c
Blair Wishom <blwishom@gmail.com>
2023-06-12 19:22:57 -0700

    join tables sql

--- Practice/join_tables.sql
+++ Practice/join_tables.sql
@@ -8,3 +8,17 @@ FROM orders
 JOIN subscriptions
   ON orders.subscription_id = subscriptions.subscription_id
 WHERE description = 'Fashion Magazine';
+
+
+
+
+SELECT count(*)
+FROM newspaper;
+
+SELECT count(*)
+FROM online;
+
+SELECT count(*)
+FROM newspaper
+JOIN online
+  ON newspaper.id = online.id;
