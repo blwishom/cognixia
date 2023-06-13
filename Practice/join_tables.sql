f8f6b5552759f36db32d96714658f95761ade0ac
Blair Wishom <blwishom@gmail.com>
2023-06-12 19:28:13 -0700

    inner join tables sql

--- Practice/join_tables.sql
+++ Practice/join_tables.sql
@@ -37,3 +37,12 @@ FROM newspaper
 LEFT JOIN online
   ON newspaper.id = online.id
 WHERE online.id IS NULL;
+
+
+
+
+
+SELECT *
+FROM classes
+INNER JOIN students
+  ON classes.id = students.class_id;
