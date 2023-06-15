c8d6900cfaefab49999adc4ad89e683d61a3cab8
Blair Wishom <blwishom@gmail.com>
2023-06-15 09:12:26 -0700

    sql window functions

--- Practice/window_functions.sql
+++ Practice/window_functions.sql
@@ -24,3 +24,34 @@ FROM
    streams
 WHERE
    artist = 'Lady Gaga';
+
+
+
+
+
+SELECT
+   artist,
+   week,
+   streams_millions,
+   LEAD(streams_millions, 1) OVER (
+      PARTITION BY artist
+      ORDER BY week
+   ) - streams_millions AS 'streams_millions_change'
+FROM
+   streams;
+
+SELECT
+   artist,
+   week,
+   streams_millions,
+   LEAD(streams_millions, 1) OVER (
+      PARTITION BY artist
+      ORDER BY week
+   ) - streams_millions AS 'streams_millions_change',
+   chart_position,
+   chart_position - LEAD(chart_position, 1) OVER (
+      PARTITION BY artist
+      ORDER BY week
+) AS 'chart_position_change'
+FROM
+   streams;
