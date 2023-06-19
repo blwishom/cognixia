e1a8c670995bbbcad123b9ec7e525a8b7393f7cb
Blair Wishom <blwishom@gmail.com>
2023-06-18 21:20:24 -0700

    oop classes method

--- Practice/py_classes.py
+++ Practice/py_classes.py
@@ -6,9 +6,9 @@ class Dog:
     self.is_friendly = friendliness
 
   def __repr__(self):
-    description = "This {breed} named {name} is {age} years old and has {number_of_friends} friends.".format(breed = self.breed, name = self.name, age=self.age, number_of_friends=len(self.friends))
+    description = "This {breed} named {name} is {age} years old. ".format(breed = self.breed, name = self.name, age=self.age)
     if self.is_friendly:
-      description += f"{name} is a friendly dog."
+      description += f"{self.name} is a friendly dog."
     else:
       description += "{name} is an unfriendly dog.".format(name = self.name)
     return description
@@ -16,14 +16,24 @@ class Dog:
 dog1 = Dog("Barker", "Husky", 3)
 dog2 = Dog("Rocky", "Cane Corso", 7, False)
 
+print(dog1)
+print(dog2)
 
 
 
 class Turtle:
-  def __init__(self, name, age, speed):
-    self.name = name,
-    self.age = age,
-    self.speed = speed
+    def __init__(self, name, age, speed):
+        self.name = name
+        self.age = age
+        self.speed = speed
+
+    def __repr__(self):
+        description = f"{self.name} is {self.age} years old and move at a {self.speed} pace."
+        return description
+
 
 Turtle1 = Turtle('Pokey', 86, 'slow')
 Turtle2 = Turtle('Speedy', 27, 'medium')
+
+print(Turtle1)
+print(Turtle2)
