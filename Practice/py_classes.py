60ce7b7b0660c8b0c15e62d3e09d456502654d6d
Blair Wishom <blwishom@gmail.com>
2023-06-18 21:00:51 -0700

    oop classes

--- Practice/py_classes.py
+++ Practice/py_classes.py
@@ -4,3 +4,15 @@ class Dog:
     self.breed = breed
     self.age = age
     self.is_friendly = friendliness
+
+dog1 = Dog("Barker", "Husky", 3)
+dog2 = Dog("Rocky", "Cane Corso", 7, False)
+
+class Turtle:
+  def __init__(self, name, age, speed):
+    self.name = name,
+    self.age = age,
+    self.speed = speed
+
+Turtle1 = Turtle('Pokey', 86, 'slow')
+Turtle2 = Turtle('Speedy', 27, 'medium')
