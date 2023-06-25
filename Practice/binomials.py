09b2b9e7576c8bbefdad200dfd9b16068e5aaf02
Blair Wishom <blwishom@gmail.com>
2023-06-25 15:18:58 -0700

    binomial

--- Practice/binomials.py
+++ Practice/binomials.py
@@ -2,7 +2,15 @@ import codecademylib
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+#generating random numbers based on probability
 emails = np.random.binomial(500, .05, size=10000)
 
 plt.hist(emails)
 plt.show()
+
+#generating probability of email responses based on specific numbers
+no_emails = np.mean(emails==0)
+print(no_emails)
+b_test_emails = np.mean(emails==8)
+print(b_test_emails)
