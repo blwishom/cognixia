390d4c10f7e50450e2370e55d43ad21ed4818ebf
Blair Wishom <blwishom@gmail.com>
2023-07-05 16:53:37 -0700

    dataframe add rows and columns

--- Practice/dataframes.py
+++ Practice/dataframes.py
@@ -5,3 +5,18 @@ df1 = pd.DataFrame({
 })
 
 print(df1)
+
+
+df2 = pd.DataFrame([
+  [1, 'San Diego', 100],
+  [2, 'Los Angeles', 120],
+  [3, 'San Francisco', 90],
+  [4, 'Sacramento', 115]
+],
+  columns=[
+    'Store ID',
+    'Location',
+    'Number of Employees'
+  ])
+
+print(df2)
