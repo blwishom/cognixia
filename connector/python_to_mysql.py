6e3399df696634b287b23b7052a9ecb3e7880eb2
Blair Wishom <blwishom@gmail.com>
2023-07-07 16:38:58 -0700

    sql executions

--- connector/python_to_mysql.py
+++ connector/python_to_mysql.py
@@ -25,10 +25,9 @@ my_cursor = my_db.cursor()
 # my_cursor.execute('SHOW TABLES')
 
 insert_students = 'INSERT INTO students (name, age) VALUES (%s, %s)'
-# update_students = 'UPDATE students SET name=%s'
-# delete_students = 'DELETE FROM students WHERE name LIKE "%s"'
+select_students = 'SELECT * FROM students'
 
-student1 = ('Jimmy', 22)
+# student1 = ('Jimmy', 22)
 # student2 = ('Linwood', 22)
 # student3 = ('Paula', 19)
 # student4 = ('Vanessa', 21)
@@ -41,13 +40,15 @@ students = [('Rodney', 21),
 # INSERT UPDATE and DELETE executions
 # my_cursor.execute(insert_students, student1)
 # my_cursor.executemany(insert_students, students)
-# my_cursor.execute('UPDATE students SET name="Ben" WHERE name LIKE "Jimmy"')
+# my_cursor.execute('INSERT INTO students (name, age) VALUES ("Thadeus", 21)')
+# my_cursor.execute('UPDATE students SET name="Wendy" WHERE name LIKE "Paula"')
 # my_cursor.execute('DELETE FROM students WHERE name LIKE "Jimmy"')
 
 # my_cursor.execute('DESCRIBE students')
-my_cursor.execute('SELECT * FROM students')
-for db in my_cursor:
-    print(db)
+my_cursor.execute(select_students)
+
+for x in my_cursor:
+    print(x)
 # for tb in my_cursor:
 #     print(tb)
 
