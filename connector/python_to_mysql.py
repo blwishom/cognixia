f2c0aadd596ce789433815a90536714525955dd2
Blair Wishom <blwishom@gmail.com>
2023-07-07 13:17:32 -0700

    populate table

--- connector/python_to_mysql.py
+++ connector/python_to_mysql.py
@@ -21,10 +21,17 @@ my_cursor = my_db.cursor()
 
 # my_cursor.execute('CREATE DATABASE if not exists test_db')
 # my_cursor.execute('SHOW DATABASES')
-my_cursor.execute('CREATE TABLE if not exists students (name VARCHAR(255), age INT(10))')
-my_cursor.execute('SHOW TABLES')
+# my_cursor.execute('CREATE TABLE if not exists students (name VARCHAR(255), age INT(10))')
+# my_cursor.execute('SHOW TABLES')
+# my_cursor.execute('INSERT INTO students (name, age) VALUES (%s, %s)')
+# my_cursor.execute('DESCRIBE students')
+student1 = ('Jimmy', 22)
+student2 = ('Linwood', 22)
+student3 = ('Paula', 19)
+student4 = ('Vanessa', 21)
+print(student1)
 
 # for db in my_cursor:
 #     print(db)
-for tb in my_cursor:
-    print(tb)
+# for tb in my_cursor:
+#     print(tb)
